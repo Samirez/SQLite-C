f020ae82801f63889ecc5d2fd9089cfa2adaf735
Ahmadullah Naibi <ahmadullah@hotmail.dk>
2026-04-22 11:04:39 +0200

    updated main to handle the new functions

--- main.c
+++ main.c
@@ -34,6 +34,28 @@ int main(int argc, char *argv[]) {
         command = cmd_buf;
     }
 
+    db_file_t db;
+    if (db_open(&db, db_file_path) != 0) {
+        fprintf(stderr, "Failed to open database file: %s\n", db_file_path);
+        return 1;
+    }
+
+    db_file_t db_header;
+    if (db_read_header(&db_header, db_file_path) != 0) {
+        fprintf(stderr, "Failed to read database header\n");
+        return 1;
+    }
+
+    db_page_t main_page;
+    if (db_read_page_t(&db, 1, &main_page) != 0) {
+        fprintf(stderr, "Failed to read main page\n");
+        db_close(&db);
+        return 1;
+    }
+
+    schema_entry_t *tables = NULL;
+    const size_t tables_count = db_load_schema(&db, &tables);
+
    if (strcmp(command, ".dbinfo") == 0)
     {
         int schema_read_result = read_sqlite_schema(db_file_path);
@@ -44,20 +66,34 @@ int main(int argc, char *argv[]) {
     }
     else if (strcmp(command, ".tables") == 0)
     {
-        int tables_read_result = print_user_name_tables(db_file_path);
-        if (tables_read_result != 0) {
-            fprintf(stderr, "print_user_name_tables failed with code %d\n", tables_read_result);
-            return 1;
+        qsort(tables, tables_count, sizeof(schema_entry_t), cmp_table_by_table_name);
+        for (size_t i = 0; i < tables_count; i++) {
+            printf("%s\n", tables[i].tbl_name);
         }
+        return 0;
     } 
     else if (strcmp(command, ".schema") == 0) {
         // not implemented yet, but we can read the schema page and print its raw content for demonstration.
         return 1;
     } 
     else {
-        fprintf(stderr, "Unknown command %s\n", command);
-        return 1;
-    }
+        const char *last_space = strrchr(command, ' ');
+        const char *table_name = last_space + 1;
 
+        for (size_t i = 0; i < tables_count; i++)
+        {
+            if (strcmp(tables[i].tbl_name, table_name) == 0)
+            {
+                db_page_t table_page;
+                if (db_read_page_t(&db, tables[i].root_page, &table_page) != 0)
+                {
+                    fprintf(stderr, "Failed to read table page for table: %s\n", tables[i].tbl_name);
+                    return 1;
+                }
+                printf("%zu\n", table_page.header.cell_count);
+            }
+        }
+    }
+    db_close(&db);
     return 0;
 }
